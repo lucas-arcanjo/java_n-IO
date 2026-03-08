3f194cc51b3e61470387e834c1b20496a519cbe4
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2026-03-08 19:22:05 -0300

    feat: write files

--- src/java_nIO/Program.java
+++ src/java_nIO/Program.java
@@ -2,21 +2,27 @@ package java_nIO;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
 
-        File diretorio = new File("/home/arcanjo/learning/files");
-        System.out.println("Diretorio existe? " + diretorio.exists());
-        if (!diretorio.exists()) {
-            diretorio.mkdirs();
-            System.out.println("Criando um diretorio." + diretorio.exists());
-        }
         try {
-            File arquivo = new File(diretorio, "arquivo.txt");
-            arquivo.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
+        Path path = Paths.get("/home/arcanjo/learning/arquivo.txt");
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Lucas");
+        nomes.add("Aline");
+
+        StringBuilder conteudo = new StringBuilder();
+        nomes.forEach(c -> conteudo.append(c + "\n"));
+        Files.write(path, conteudo.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
