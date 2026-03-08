218793c3bf1a0fecfaa29cc3bbcf86f57bfb85b9
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2026-03-07 21:02:01 -0300

    feat: adding file

--- src/java_nIO/Program.java
+++ src/java_nIO/Program.java
@@ -1,7 +1,22 @@
 package java_nIO;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Program {
     public static void main(String[] args) {
 
+        File diretorio = new File("/home/arcanjo/learning");
+        System.out.println("Diretorio existe? " + diretorio.exists());
+        if (!diretorio.exists()) {
+            diretorio.mkdir();
+            System.out.println("Criando um diretorio." + diretorio.exists());
+        }
+        try {
+            File arquivo = new File(diretorio, "arquivo.txt");
+            arquivo.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
